f859b426d333a3e56410f4deadb376dafa3e4333
noti0na1 <noti0na1@users.noreply.github.com>
2020-09-23 02:44:21 -0400

    Update tests

---
+++ tests/explicit-nulls/neg/unsafe-scope.scala
@@ -0,0 +1,18 @@
+class S {
+  given Conversion[String, Array[String]] = _ => ???
+
+  def f = {
+    val s: String | Null = ???
+
+    val x: String = s // error
+    val xs: Array[String | Null] = s // error
+    {
+      import scala.language.unsafeNulls
+      val y: String = s
+      val ys: Array[String | Null] = s
+    }
+
+    val z: String = s // error
+    val zs: Array[String | Null] = s // error
+  }
+}
