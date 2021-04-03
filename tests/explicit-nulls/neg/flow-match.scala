0e1f277e78430d07f132ea12ba42ed2c511aeb50
noti0na1 <noti0na1@users.noreply.github.com>
2021-04-02 22:18:33 -0400

    Add test for i11967

--- tests/explicit-nulls/neg/flow-match.scala
+++ tests/explicit-nulls/neg/flow-match.scala
@@ -2,14 +2,16 @@
 
 object MatchTest {
 
-  def lengthOfStringOrNull(s: String | Null): Int = s match {
-    case _: String => s.length
-    case _ => 0
-  }
-
   def stringOrNullToString(s: String | Null): String = s match {
     case null => "null"
     // after the null case, s becomes non-nullable
     case _ => s
+  }
+
+  def stringOrNullToString2(s: String | Null): String = s match {
+    case null => "null"
+    // s in the case is new variable different from the paramter s
+    // it has the original type: String | Null
+    case s => s // error
   }
 }
